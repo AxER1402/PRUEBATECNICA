02c813a4a6ec8ef731fe5c1ff3dcbd3e635abeba
AxER1402 <jeffersonperezblanco22@gmail.com>
2024-06-20 19:27:09 -0600

    Proyecto completo

---
+++ PRUEBATECNICA/GestorDeContactos.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+//Crear la lista 
+public class GestorDeContactos
+{
+    private List<Contacto> contactos;
+    private string filePath = "contactos.txt";
+
+    public GestorDeContactos()
+    {
+        contactos = new List<Contacto>();
+        CargarContactos();
+    }
+    //Agregar
+    public int AgregarContacto(Contacto contacto)
+    {
+        contacto.Id = contactos.Count > 0 ? contactos[contactos.Count - 1].Id + 1 : 1;
+        contactos.Add(contacto);
+        GuardarContactos();
+        return contacto.Id;
+    }
+
+    //Buscar
+    public List<Contacto> BuscarContactos(string criterio)
+    {
+        return contactos.FindAll(c => c.Nombre.IndexOf(criterio, StringComparison.OrdinalIgnoreCase) >= 0 ||
+                                      c.Telefono.IndexOf(criterio, StringComparison.OrdinalIgnoreCase) >= 0);
+    }
+
+
+    public List<Contacto> ListarContactos()
+    {
+        return new List<Contacto>(contactos);
+    }
+    //Eliminar
+    public void EliminarContacto(int id)
+    {
+        contactos.RemoveAll(c => c.Id == id);
+        GuardarContactos();
+    }
+    //Guardar
+    private void GuardarContactos()
+    {
+        using (StreamWriter sw = new StreamWriter(filePath))
+        {
+            foreach (var contacto in contactos)
+            {
+                sw.WriteLine($"{contacto.Id},{contacto.Nombre},{contacto.Telefono},{contacto.Email}");
+            }
+        }
+    }
+    //Subida
+    private void CargarContactos()
+    {
+        if (File.Exists(filePath))
+        {
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    var parts = line.Split(',');
+                    if (parts.Length == 4)
+                    {
+                        Contacto contacto = new Contacto
+                        {
+                            Id = int.Parse(parts[0]),
+                            Nombre = parts[1],
+                            Telefono = parts[2],
+                            Email = parts[3]
+                        };
+                        contactos.Add(contacto);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ PRUEBATECNICA/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Program
+{
+    //}Diseño de la interfaz
+    static void Main(string[] args)
+    {
+        GestorDeContactos gestor = new GestorDeContactos();
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("1. Agregar nuevo contacto");
+            Console.WriteLine("--------------------------------------------------------------------------------------------------------------");
+            Console.WriteLine("2. Buscar contactos");
+            Console.WriteLine("--------------------------------------------------------------------------------------------------------------");
+
+            Console.WriteLine("3. Mostrar todos los contactos");
+            Console.WriteLine("--------------------------------------------------------------------------------------------------------------");
+
+            Console.WriteLine("4. Eliminar contactos");
+            Console.WriteLine("--------------------------------------------------------------------------------------------------------------");
+
+            Console.WriteLine("5. Salir ");
+            Console.WriteLine("--------------------------------------------------------------------------------------------------------------");
+
+            Console.Write("Ingrese un número para elegir la opción: ");
+            Console.WriteLine();
+            Console.WriteLine("--------------------------------------------------------------------------------------------------------------");
+
+            int opcion = int.Parse(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1:
+                    AgregarNuevoContacto(gestor);
+                    break;
+                case 2:
+                    BuscarContactos(gestor);
+                    break;
+                case 3:
+                    ListarContactos(gestor);
+                    break;
+                case 4:
+                    EliminarContacto(gestor);
+                    break;
+                case 5:
+                    return;
+                default:
+                    Console.WriteLine("Opción no válida. Intente de nuevo.");
+                    break;
+            }
+        }
+    }
+
+    //Validación para agregar
+    private static void AgregarNuevoContacto(GestorDeContactos gestor)
+    {
+        Console.Write("Nombre: ");
+        string nombre = Console.ReadLine();
+        Console.WriteLine("--------------------------------------------------------------------------------------------------------------");
+        Console.Write("Teléfono: ");
+        string telefono = Console.ReadLine();
+        Console.WriteLine("--------------------------------------------------------------------------------------------------------------");
+
+        Console.Write("Email: ");
+        string email = Console.ReadLine();
+        Console.WriteLine("--------------------------------------------------------------------------------------------------------------");
+
+        Contacto contacto = new Contacto { Nombre = nombre, Telefono = telefono, Email = email };
+        int id = gestor.AgregarContacto(contacto);
+        Console.WriteLine($"Contacto agregado con el ID = {id}. Presione cualquier tecla para regresar al menú...");
+        Console.ReadKey();
+    }
+
+
+    //Validación para Buscar
+    private static void BuscarContactos(GestorDeContactos gestor)
+    {
+        Console.Write("Ingrese el nombre o teléfono para buscar: ");
+        string criterio = Console.ReadLine();
+        var resultados = gestor.BuscarContactos(criterio);
+
+        if (resultados.Count > 0)
+        {
+            Console.WriteLine("Contactos encontrados:");
+            foreach (var contacto in resultados)
+            {
+                Console.WriteLine(contacto);
+            }
+        }
+        else
+        {
+            Console.WriteLine("No se encontraron contactos.");
+        }
+
+        Console.WriteLine("Presione cualquier tecla para volver al menu...");
+        Console.ReadKey();
+    }
+    //Validación para mostrar todo
+    private static void ListarContactos(GestorDeContactos gestor)
+    {
+        var contactos = gestor.ListarContactos();
+
+        if (contactos.Count > 0)
+        {
+            Console.WriteLine("Lista de contactos:");
+            foreach (var contacto in contactos)
+            {
+                Console.WriteLine(contacto);
+                Console.WriteLine("--------------------------------------------------------------------------------------------------------------");
+
+
+            }
+        }
+        else
+        {
+            Console.WriteLine("No hay contactos en la lista.");
+        }
+
+        Console.WriteLine("Presione cualquier tecla para regresar al menu...");
+        Console.ReadKey();
+    }
+    //Validación para eliminar
+    private static void EliminarContacto(GestorDeContactos gestor)
+    {
+        Console.Write("Ingrese el ID del contacto a eliminar: ");
+        int id = int.Parse(Console.ReadLine());
+
+        gestor.EliminarContacto(id);
+        Console.WriteLine("Contacto eliminado. Presione cualquier tecla para regresar al menu...");
+        Console.ReadKey();
+    }
+}
